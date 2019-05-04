80ea2b5390d9db7ae5e983714956851535e2a24b
ibrahim ahmed <ibrahim.ahmed@ultratendency.com>
2019-05-04 18:12:57 +0200

    health bar

--- Assets/ScriptableObjects/ScriptableObjectFloat.cs
+++ Assets/ScriptableObjects/ScriptableObjectFloat.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,4 +8,9 @@ using UnityEngine;
 public class ScriptableObjectFloat : ScriptableObject
 {
    public float currentValue, maxValue;
+
+    public static implicit operator float(ScriptableObjectFloat v)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ Assets/Scripts/healthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour
+{
+    [SerializeField] private ScriptableObjectFloat HealthValue;
+    public Image healthImage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthImage.fillAmount = HealthValue.currentValue;
+    }
+}
