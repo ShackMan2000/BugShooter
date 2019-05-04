2ad19f31b092720bb707f423fd7987ab99b325a3
ibrahim ahmed <ibrahim.ahmed@ultratendency.com>
2019-05-04 16:26:56 +0200

    Ground scrolling

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Ground : MonoBehaviour
+{
+    // Scroll main texture based on time
+    [SerializeField] private ScriptableObjectFloat scrollSpeed;
+
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed.currentValue;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, -offset));
+    }
+}
