be4b759ecb66d7593027ce0b09393b78c4512f9e
ShackMan2000 <julianpieper@gmx.de>
2019-05-04 20:58:49 +0200

    no message

--- Assets/Scripts/healthBar.cs
+++ Assets/Scripts/healthBar.cs
@@ -6,19 +6,38 @@ public class healthBar : MonoBehaviour
 {
     [SerializeField]
     private ScriptableObjectFloat HealthValue;
-
-
+    
+    
     private Image healthImage;
+
+    private Vector3 goodKarmaScale, badKarmaScale;
+
+    private Image spriteRenderer;
 
     private void Awake()
     {
         healthImage = GetComponent<Image>();
+        goodKarmaScale = transform.localScale;
+        badKarmaScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        spriteRenderer = GetComponent<Image>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        healthImage.fillAmount = HealthValue.currentValue/ HealthValue.maxValue;
+        if (HealthValue.currentValue > 0)
+        {
+            transform.localScale = goodKarmaScale;
+            healthImage.fillAmount = HealthValue.currentValue / HealthValue.maxValue;
+        }
+        else
+        {
+            transform.localScale = badKarmaScale;
+            healthImage.fillAmount = HealthValue.currentValue / -HealthValue.maxValue;
+        }
+
+
+
     }
 }
